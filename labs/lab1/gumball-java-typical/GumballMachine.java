aa346ae4e942b6d958d4f83f47046ff5f28dfc09
Shivani Reddy Singireddy <shivanireddysingireddy@Shivanis-MacBook-Pro.local>
2019-02-08 15:36:36 -0800

    Added changes to typical probem without pattern

--- labs/lab1/gumball-java-typical/GumballMachine.java
+++ labs/lab1/gumball-java-typical/GumballMachine.java
@@ -1,34 +1,70 @@
-
+import java.util.*;
 public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    private boolean has_coin;
+    private boolean hasEnoughMoney;
+    
+    
+    List<Integer> acceptingCoins = new ArrayList<Integer>();
+    int amountPresent = 0;
+    int costOfEjection = 0;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int machineNumber )
     {
-        // initialise instance variables
+        this.has_coin = false;
+        this.hasEnoughMoney = false;
         this.num_gumballs = size;
-        this.has_quarter = false;
+        switch(machineNumber) {
+        case 1:
+            acceptingCoins.add(25);
+            costOfEjection = 25; 
+            break;
+        case 2:
+            acceptingCoins.add(25);
+            costOfEjection = 50;
+            break;
+        case 3:
+            acceptingCoins.add(5);
+            acceptingCoins.add(10);
+            acceptingCoins.add(25);
+            costOfEjection = 50;
+            break;
+        default: 
+            System.out.print("Only 3 Machines Available, choose 1 or 2 or 3");
+        }
     }
 
-    public void insertQuarter(int coin)
-    {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+    public List<Integer> getAcceptingCoins() {
+        return acceptingCoins;
     }
+    public void insertCoin(int coin)
+    {
+        if ( this.getAcceptingCoins().contains(coin)){
+            this.has_coin = true;
+            this.amountPresent += coin; 
+            if(!(this.amountPresent < this.costOfEjection)){
+                this.hasEnoughMoney = true;
+                if(this.amountPresent > this.costOfEjection) {
+                    System.out.println("You have inserted" + (this.amountPresent - this.costOfEjection) + "more , Take change!!");
+                }
+            } 
+    } else {
+        System.out.println("you have entered invalid coin");
+    }
+}
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	if ( this.hasEnoughMoney )
     	{
     		if ( this.num_gumballs > 0 )
     		{
     			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+    			this.hasEnoughMoney = false ;
+    			this.amountPresent = 0;
+    			System.out.println( "Thanks for your money.  Gumball Ejected!" ) ;
     		}
     		else
     		{
@@ -36,8 +72,17 @@ public class GumballMachine
     		}
     	}
     	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
+    	{ if(!this.has_coin) {
+    	   System.out.println( "Insert money to eject gumballs." ) ;
+    	   } 
+    	   else if(this.amountPresent < this.costOfEjection){
+    	       System.out.println("Insert enough money for gumball");
+    	   }
+    	   
+    	   
+  
     	}        
     }
+    
+    
 }
--- labs/lab1/gumball-java-typical/Main.java
+++ labs/lab1/gumball-java-typical/Main.java
@@ -2,21 +2,30 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        GumballMachine gumballMachine1 = new GumballMachine(5, 1);
 
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-	}
+        System.out.println(gumballMachine1);
+        System.out.println(".........Machine1.....");
+        //Inserting invalid coin to gumballMachine1 so that it throws error
+        gumballMachine1.insertCoin(25);
+        gumballMachine1.turnCrank();
+        
+        System.out.println(".........Machine2.....");
+        GumballMachine gumballMachine2 = new GumballMachine(5, 2);
+        //Inserting valid coin with less amount to machine 2
+        gumballMachine2.insertCoin( 25 );
+        gumballMachine2.insertCoin( 25 );
+        gumballMachine2.insertCoin( 25 );
+        gumballMachine2.turnCrank();
+        
+        System.out.println(".........Machine3.....");
+       GumballMachine gumballMachine3 = new GumballMachine(6, 3);
+        //Inserting valid coin with less amount to machine 2
+        gumballMachine3.insertCoin( 25 );
+        gumballMachine3.insertCoin( 5 );
+        gumballMachine3.insertCoin( 10);
+        gumballMachine3.insertCoin( 10);
+        gumballMachine3.turnCrank();
+    }
 }
